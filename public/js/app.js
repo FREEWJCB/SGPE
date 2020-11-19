cec384c8c53a7253246ed59e61a538f0beadbeda
Walter Caro <60450133+FREEWJCB@users.noreply.github.com>
2020-11-19 19:30:06 -0400

    se agrego en el webpack el js y sass de login

--- public/js/app.js
+++ public/js/app.js
@@ -70507,15 +70507,27 @@ var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_
 
 /***/ }),
 
+/***/ "./resources/sass/login.scss":
+/*!***********************************!*\
+  !*** ./resources/sass/login.scss ***!
+  \***********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ 0:
-/*!*************************************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/css/app.css ***!
-  \*************************************************************************************/
+/*!*****************************************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/login.scss ./resources/css/app.css ***!
+  \*****************************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! C:\Users\COLDWJCB\Documents\GitHub\SGPE\resources\js\app.js */"./resources/js/app.js");
 __webpack_require__(/*! C:\Users\COLDWJCB\Documents\GitHub\SGPE\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\COLDWJCB\Documents\GitHub\SGPE\resources\sass\login.scss */"./resources/sass/login.scss");
 module.exports = __webpack_require__(/*! C:\Users\COLDWJCB\Documents\GitHub\SGPE\resources\css\app.css */"./resources/css/app.css");
 
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,10 +11,10 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/js/app.js', 'public/js').js('resources/js/login.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require('postcss-import'),
         require('tailwindcss'),
     ])
-    .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/app.scss', 'public/css').sass('resources/sass/login.scss', 'public/css')
     .webpackConfig(require('./webpack.config'));

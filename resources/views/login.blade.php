f506c414bb71fdb16dfbee1a3ccc0f8d1af7e38f
Walter Caro <60450133+FREEWJCB@users.noreply.github.com>
2020-11-19 21:38:55 -0400

    se agrego el login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,25 @@
+
+@extends('layouts.login')
+@section('contenido')
+    <div class="box">
+        <h2>Iniciar Sesión</h2>
+        <form action="{{ route('login') }}">
+            <div class="inputBox">
+                <input type="text" required name="username" id="username">
+                <label for="username">Nombre de usuario</label>
+            </div>
+            <div class="inputBox">
+                <input type="password" required name="password" id="password">
+                <label for="password">Contraseña</label>
+            </div>
+            <div class="inputCheckbox">
+                <input id="recuerdame" type="checkbox" name="remember-me">
+                <label class="label-checkbox100" for="ckb1">Recuérdeme</label>
+            </div>
+            <div class="submit">
+                <input type="submit" value="Iniciar"><br>
+                <a href="#">¿Se te olvidó tu contraseña?</a>
+            </div>
+        </form>
+    </div>
+@endsection
